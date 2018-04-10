29424b8de738180c745c0f1ca078aa43545b1bcf
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-10 10:00:21 -0300

    test

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 var http = require('http');
-var fs = require("fs");
+var url = require('url');
+var handleRequest = require('./request-handler.js');
+var handlePostRequest = require('./post-request-hadler.js');
+var utilities = require('./utilities.js');
 
-fs.readFile('index.html', function (err, html) {
+var routes = {
+    '/login': handleRequest,
+    '/submitEmpDetails': handlePostRequest
+};
 
-    var server = http.createServer(function(request, response) {
-        
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.end(html);
-        
+var server = http.createServer(function(request, response) {
+    
+    //response.writeHead(200, {"Content-Type": "text/html"});
+    //response.end(html);
+
+    var body = '';
+    
+    request.on('data', function (data) {
+        body += data;
     });
-        
-    var port = process.env.PORT || 1337;
-    server.listen(port);
-        
-    console.log("Server running at http://localhost:%d", port);
 
+    request.on('end', function() {
+
+        console.log("BODY: " + body);
+        
+        var urlParts = url.parse(request.url);
+        var route = routes[urlParts.pathname];
+        
+        if(route && request.method == "POST") {
+            route(request, response, body);
+        }
+          
+        if (route) route(request, response);
+        else utilities.sendResponse(response, 'Not Found', 404);
+
+    });
+    
 });
+    
+var port = process.env.PORT || 1337;
+server.listen(port);
+    
+console.log("Server running at http://localhost:%d", port);
